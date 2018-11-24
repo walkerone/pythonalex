b23dd07a268a741b90f4874c5a8fc0e66bec6772
walkerone <zzq1401660184@163.om>
2018-11-24 22:07:57 +0800

    test

---
+++ day1119/shpping.py
@@ -0,0 +1,38 @@
+# -*- coding:utf-8 -*-
+product_list = [
+    ('Iphone', 5800),
+    ('Mac Pro', 9800),
+    ('Bike', 800),
+    ('Watch', 10600),
+    ('Coffee', 31),
+    ('Alex Python', 120),
+]
+shopping_list = []
+salary = input("Input your salary:")
+
+if salary.isdigit():
+    salary = int(salary)
+    while True:
+        for index, item in enumerate(product_list):
+            print(index, item)
+        user_choice = input("what you want buy")
+        if user_choice.isdigit():
+            user_choice = int(user_choice)
+            if user_choice > 0 and user_choice < len(product_list):
+                p_item = product_list[user_choice]
+                if p_item[1] < salary:
+                    print("买得起呀")
+                    shopping_list.append(p_item)
+                    salary -= p_item[1]
+                    print("Added %s into shopping cart,your current balance is \033[31;1m%s\033[0m" % (p_item, salary))
+                    print("added %s into shopping cart,your current balance is\033[31;1m%s\033[0m" % (p_item, salary))
+                else:
+                    print("你的金额只剩\033[31;1m%s\033[0m" % salary)
+            else:
+                print("produce \033[31:m code\033[0:m is not here")
+        elif user_choice == "q":
+            print("--------shppintlist----------")
+            print(shopping_list)
+            exit()
+        else:
+            print("valid option")
--- day1119/sys_test.py
+++ day1119/sys_test.py
@@ -1,8 +1,25 @@
 # -*- coding:utf-8 -*-
 import sys,os
+import copy
 """print(sys.argv) #打印环境变量
 # print(sys.argv[2])# 取出第几个参数
 """
 # cmd_result=os.system("dir")  #执行命令,不保存结果
 cmd_res=os.popen("dir").read() #先read下然后赋值给变量
-cmd_res.read()
+print(cmd_res)
+
+name=["walker","zhang",["dog","cat"]]
+namer2=copy.copy(name)  #浅浅的copy只是copy了最外面一层
+#copy中的copy和列表中的copy是一样的，深copy是deepcopy
+
+
+print(name)
+print(namer2)
+name[0]="walker123"
+name[2][0]="Dog"
+print(name)
+print(namer2)
+列表浅copy的三种方式
+p1=copy.copy(name)
+p1=name.copy()
+p1=name[:]
