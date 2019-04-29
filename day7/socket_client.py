3a829f666dadceee13d0a259dfa54379bc2b24d3
walkerone <zzq1401660184@163.com>
2019-04-29 21:56:14 +0800

    test

--- day7/socket_client.py
+++ day7/socket_client.py
@@ -3,11 +3,12 @@ import socket
 client=socket.socket()  #声明socket 类型，同时生成sockent连接对象
 client.connect(('localhost',6969))
 while True:
-    msg=input("---->").strip()
-    client.send(msg.encode()) #所有数据的传输都是以btye类型进行
-    data=client.recv(1024)
+    cmd=input("---->").strip()
+    if len(cmd)==0:continue
+    client.send(cmd.encode()) #所有数据的传输都是以btye类型进行
+    cmd_res=client.recv(1024)
     with open("dir.txt",'wb') as f1:
-        f1.write(data)
+        f1.write(cmd_res)
         f1.flush()
-    print('recv',data.decode())
+    print('recv',cmd_res.decode())
 client.close()
--- day7/socket_server.py
+++ day7/socket_server.py
@@ -1,23 +1,31 @@
 # -*- coding:utf-8 -*-
 import  socket
 import os
-server=socket.socket()
-server.bind(('localhost',6969))
-server.listen()
+server=socket.socket()  #实例
+server.bind(('localhost',6969))  #绑定
+server.listen()   #监听
 print("开始等电话")
-conn,addr=server.accept()  #等电话
+
 #conn 就是客户端连接过来而在服务器为其生成的一个连接实例
-print(conn,addr)
+
 while True:
-    data=conn.recv(1024)
-    if not data:
-        print("client have lost")
-        break
-    print(data.decode('utf-8'))
-    data=os.popen(data.decode('utf-8'))  #popen执行命令
-    res=data.read()
-    conn.send(res.encode())
-    conn.sendall() #相当于for 循环进行send
+    conn,addr=server.accept()
+    print(conn)
+    while True:
+        print("等待新的指令")
+        data=conn.recv(1024)
+        if not data:
+            print("client 已断开")
+            break
+        print(data.decode('utf-8'))
+        data=os.popen(data.decode('utf-8'))  #popen执行命令,接收的是字符串，执行结果也是字符串
+        cmd_res=data.read()
+        print(len(cmd_res))
+        if len(cmd_res)==0:
+            cmd_res=b"cmd has no output"
+        conn.send(cmd_res.encode())
+        print("send done")
+    # conn.sendall() #相当于for 循环进行send
 server.close()
 
 
